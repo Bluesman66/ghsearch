e8d4fa07d38fc23728a8a775a234f5316f29ab8b
alex.paclin <alex.paclin@gmail.com>
2018-05-16 00:26:36 +0500

    github service

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from './../../services/github.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private githubService: GithubService) {
+    this.githubService.getUser()
+      .subscribe(user => {
+        console.log(user);
+      });
+  }
 
   ngOnInit() {
   }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {  
+  private username: string = 'Bluesman66';
+  private client_id = 'd19371d3a2826f290cae';
+  private client_secret = 'c91d2f649a0917ef750fcfba374bc1db0ad609f7';
+
+  constructor(private http: Http) {
+    console.log('Github Service Ready...');
+  }
+
+  getUser(){
+    return this.http.get(`http://api.github.com/users/${this.username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+      .pipe(map(res => res.json()));      
+  }
+}
